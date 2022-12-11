0ae1662be02c2190cd6dc634c90847605b847e1e
Novitskii Nikita <novitskiyn1@gmail.com>
2022-12-11 15:24:45 +0300

    addWorkers css

--- server/src/routers/calendar.router.js
+++ server/src/routers/calendar.router.js
@@ -16,6 +16,7 @@ calendarRouter.get('/', async (req, res) => {
 calendarRouter.post('/', async (req, res) => {
   try {
     const workerId = req.session.company.id;
+    const { title, start, end } = req.body;
     await Tasks.create({
       title, start, end, worker_id: workerId, creator_id: workerId, task_type: 'personal',
     });
