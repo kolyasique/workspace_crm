50740e0797b6c8a735c8e9cd502163f811bd4198
Novitskii Nikita <novitskiyn1@gmail.com>
2022-12-13 12:53:56 +0300

    btn client

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/User.context';
@@ -13,11 +13,11 @@ export default function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const {
-    dateNow, convertDate1, mainOrProfile, setMainOrProfile
+    dateNow, convertDate1, mainOrProfile, setMainOrProfile,
   } = useContext(UserContext);
-  
+  console.log('🚀🚀🚀🚀 =>>>>> file: Header.js:18 =>>>>> Navbar =>>>>> mainOrProfile', mainOrProfile);
+
   const { user } = useSelector((store) => store.userStore);
-  console.log('🚀🚀🚀🚀 =>>>>> file: Header.js:17 =>>>>> Navbar =>>>>> user', user);
 
   const handleLogout = useCallback(() => {
     fetch(
@@ -52,7 +52,14 @@ export default function Navbar() {
         <>
           <img className={cl.navlogo} src={logoWS} alt="workspace" />
           <div />
-          <button className={cl.logoutBtn} type="button" onClick={handleLogout}>Выйти</button>
+          <div className={cl.menuBtns}>
+            {mainOrProfile ? (
+              <button type="button" className={cl.logoutBtn} onClick={handleButtonChange}>Клиенты</button>
+            ) : (
+              <button type="button" className={cl.logoutBtn} onClick={handleButtonChange}>Сотрудники</button>
+            )}
+            <button className={cl.logoutBtn} type="button" onClick={handleLogout}>Выйти</button>
+          </div>
         </>
       )}
 
